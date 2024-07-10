0854584f38942663429d4fefeb0444fa2a627f91
anikmondol <anikmondol558363@gmail.com>
2024-07-10 18:24:08 +0600

    added this 07_data_taypes.php file in this github projects

---
+++ 07_data_types.php
@@ -0,0 +1,79 @@
+<?php
+
+$name = "anik";
+$income = 250;
+
+
+/*
+
+***---> php data types
+
+1. String
+2. Integer
+3. Float
+4. Boolean
+5. Object
+6. Array
+7. Null
+
+*/ 
+
+// 1. Strain ---> sequence of characters
+
+// $name = "anik";
+// $friend = "apu";
+
+// echo "$name ar friend hoicay $friend ";
+
+
+// 2. Integer ---> Non decimal number
+
+// $income = 202;
+// $debts = -122;
+
+// echo $income . "<br>";
+// echo $debts;
+
+
+// 3. Float ---> Decimal point number
+
+// $income = 472.4545;
+// $debts = -42.241;
+
+// echo $income . "<br>";
+// echo $debts;
+
+
+// 4. Boolena ---> Can be either true or false
+
+// $x = true;
+// $is_friend = false;
+
+// echo var_dump($x) ."<br>";
+// echo var_dump($is_friend);
+
+
+
+//5. Object ---> Instances of classes
+// Employee is a class ---> harry can be one object
+
+
+
+//6. Array ---> Used to store multiple values in a single variables
+
+// $friend = array("anik", "apu", "joy", "arpon");
+// echo var_dump($friend);
+// echo $friend[0] . "<br>";
+// echo $friend[1] . "<br>";
+// echo $friend[2] . "<br>";
+// echo $friend[3] . "<br>";
+// echo $friend[4] . "<br>"; // will throw an error as the size of array is 4
+
+
+// 7. NULL
+
+$name = NULL;
+// echo $name;
+// echo var_dump($name);
+
+?>
