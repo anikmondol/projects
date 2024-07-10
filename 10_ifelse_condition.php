57df0adb1bde7c08c5fd73c2977b04d9fda40844
anikmondol <anikmondol558363@gmail.com>
2024-07-10 22:20:03 +0600

    added if, elseif, else program file

---
+++ 10_ifelse_condition.php
@@ -0,0 +1,41 @@
+<?php
+
+// $a = 665;
+// $b = 20;
+
+
+// if($a > 30){
+//     echo "a is greater than 78";
+// }else{
+//     echo "a is not greater than 78";
+// }
+
+//  $age = 15;
+
+//  If else lader
+//  if($age >= 18){
+//     echo "You can drink water with chai and alcohol";
+//  }elseif($age >= 13){
+//     echo "You can drink chai only with water. No alcohol for you";
+//  }else{
+//     echo "You can drink water only";
+//  }
+
+
+// hw
+
+$age = array(22, 33);
+
+if($age >= 25 && $age <= 65){
+    echo "you can allow drive";
+}elseif ($age < 25) {
+    echo "you are child";
+}elseif ($age > 65) {
+    echo "you are old";
+}else{
+    echo "you can not allow drive";
+}
+
+
+
+?>
