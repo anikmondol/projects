13b19b4fe2c176da480e3b9cefda2b4cb2505d37
anikmondol <anikmondol558363@gmail.com>
2024-07-10 15:10:57 +0600

    creat project fill in this php crouse

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+
+#echo "This is Tutorial 5";
+# variables are containers for storing information
+# starts with a $
+$name = "anik";
+# variable are case sensitive
+$namE = "Capital wala anik";
+$income = 200;
+
+echo "This guy is $name and his income is  tk. $income <br>";
+echo "$namE is a good boy <br>";
+echo "$name is the real gengstar <br>"
+
+?>
