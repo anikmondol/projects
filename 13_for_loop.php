e68829d1bf4ded1ce6bfa75ac92c2d3a4b73db3b
anikmondol <anikmondol558363@gmail.com>
2024-07-10 23:14:17 +0600

    added this code

---
+++ 13_for_loop.php
@@ -0,0 +1,16 @@
+<?php
+
+// echo "This is for loop in action <br>";
+
+// for($i = 0; $i <= 10; $i++){
+//     // for(initialization; condition; updation)
+//     echo $i . "<br>";
+// }
+
+
+for ($i=0; $i < 50; $i++) { 
+    echo $i . "<br>";
+}
+
+
+?>
