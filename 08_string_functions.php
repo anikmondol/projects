e94db35f37b2a6d0efe07079ae61ea338bb62766
anikmondol <anikmondol558363@gmail.com>
2024-07-10 18:50:00 +0600

    added 08_string_functions.php file in this github

---
+++ 08_string_functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+$name = "  anik is a good boy   ";
+// echo $name . "<br>";
+
+// echo "The length of my string is " . strlen($name) . "<br>";
+// echo str_word_count($name) . "<br>";
+// echo strrev($name) . "<br>";
+// echo strpos($name , "good") . "<br>";
+// echo str_replace("good", "bad", $name) . "<br>";
+// echo str_repeat($name, 4) . "<br>";
+// echo "<pre>";
+// echo ltrim("     anik is my name        ");
+// echo "</pre>";
+// echo $name;
+
+
+
+
+?>
