1a6e1d9f9e68a893f863e09dd303c1075eac4ba3
anikmondol <anikmondol558363@gmail.com>
2024-07-10 19:44:06 +0600

    added thsi file in this github

---
+++ 09_operators.php
@@ -0,0 +1,101 @@
+<?php
+/*
+***-----> Operators in PHP
+
+1. Arithmetice Operators
+2. Assigment Operators
+3. Comparison Operators
+4. Logical Operators
+
+*/
+
+
+
+
+// 1. Arithmetice Operators
+
+
+// $a = 45;
+// $b = 8;
+
+// echo "For a + b, the result is = " . ($a + $b) . "<br>";
+// echo "For a - b, the result is = " . ($a - $b) . "<br>";
+// echo "For a * b, the result is = " . ($a * $b) . "<br>";
+// echo "For a / b, the result is = " . ($a / $b) . "<br>";
+// echo "For a % b, the result is = " . ($a  % $b) . "<br>";
+// echo "For a ** b, the result is = " . ($a ** $b) . "<br>";
+
+
+// 2. Assigment Operators
+
+
+// $x = $a;
+// echo "For x, the value is " . $x . "<br>";
+
+// $a += 6;
+// echo "For a, the value is ". $a . "<br>";
+
+// $a -= 6;
+// echo "For a, the value is ". $a . "<br>";
+
+
+// $a *= 6;
+// echo "For a, the value is ". $a . "<br>";
+
+// $a /= 6;
+// echo "For a, the value is ". $a . "<br>";
+
+// $a %= 6;
+// echo "For a, the value is ". $a . "<br>";
+
+
+
+
+// 3. Comparison Operators
+
+$x = 7;
+$y = 77;
+
+// echo "For x == y, the result is = ";
+// echo var_dump($x == $y) . "<br>";
+
+
+// echo "For x > y, the result is = ";
+// echo var_dump($x > $y) . "<br>";
+
+// echo "For x <= y, the result is = ";
+// echo var_dump($x <= $y) . "<br>";
+
+
+
+
+// echo "For x < y, the result is = ";
+// echo var_dump($x < $y) . "<br>";
+
+
+// echo "For y >= x, the result is = ";
+// echo var_dump($y >= $x) . "<br>";
+
+
+// echo "For x != y, the result is = ";
+// echo var_dump($y != $x) . "<br>";
+
+
+
+// 4. Logical Operators
+
+$m = false;
+$n = false;
+
+// echo "For m and n, the result is ";
+// echo var_dump($m and $n);
+
+// echo "For m or n, the result is ";
+// echo var_dump($m or $n);
+
+// echo "For m and n, the result is ";
+// echo var_dump(!$m);
+
+
+
+?>
